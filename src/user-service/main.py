df47913b065a6c536cd2c3b1f6a420e0125ff2c6
Shin <shin910113@wke.csie.ncnu.edu.tw>
2024-11-09 19:37:34 +0800

    feat(app): add cors

--- src/user-service/main.py
+++ src/user-service/main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
 from router import router, privtate_router
 from exception import add_global_exception_handler
 from schemas import ErrorResponse
+from config import FRONTEND_URL
 import uvicorn
 
-app = FastAPI(
-    responses={status.HTTP_400_BAD_REQUEST: {"model": ErrorResponse}})
+origins = [FRONTEND_URL]
+
+
+app = FastAPI(responses={status.HTTP_400_BAD_REQUEST: {"model": ErrorResponse}})
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 add_global_exception_handler(app)
 app.include_router(router)
 app.include_router(privtate_router)
